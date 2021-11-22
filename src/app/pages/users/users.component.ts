b14a62fe8a4919300830bc3590784b9416fc7ba5
Igor Faoro <igor.faoro17@gmail.com>
2021-11-22 18:37:51 -0300

    delete

--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -2,10 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { User } from '../../models/api/user';
 import { UserFilter } from '../../models/output/filters/user.filter';
+import { DialogMessageOptions } from '../../models/ui/dialog-message-options';
 import { LoaderService } from '../../services/loader.service';
 import { TitleService } from '../../services/title.service';
 import { ToastService } from '../../services/toast.service';
 import { UserService } from '../../services/user.service';
+import { DialogMessageComponent } from '../../shared/dialog-message/dialog-message.component';
 import { UserFormComponent, UserFormInputData } from './user-form/user-form.component';
 
 @Component({
@@ -26,8 +28,6 @@ export class UsersComponent implements OnInit {
     'id',
     'code',
     'name',
-    'birthDate',
-    'createdAt',
     'profileImage',
     'options'
   ];
@@ -92,6 +92,10 @@ export class UsersComponent implements OnInit {
     });
   }
 
+  public openDetails(userId: number): void {
+
+  }
+
   public openForm(userId?: number): void {
 
     const data: UserFormInputData = { userId }
@@ -102,5 +106,36 @@ export class UsersComponent implements OnInit {
     });
 
     dialog.afterClosed().subscribe(() => this.getUsers({ reset: true, showLoader: false }));
+  }
+
+  public deleteUser(user: User): void {
+
+    const data: DialogMessageOptions = {
+      message: `Are you sure you want to delete the "${user.name}"?`,
+      title: 'Delete',
+      buttons: [
+        {
+          text: 'No',
+          closeOnClick: true
+        },
+        {
+          text: 'Yes',
+          color: 'warn',
+          onClick: () => {
+            this._loader.show();
+            this._userService.delete(user.id).subscribe(response => {
+              this._loader.dismiss();
+              this._toast.open('Deleted successfully!');
+              this.getUsers({ reset: true, showLoader: false });
+            }, error => {
+              this._loader.dismiss();
+              this._toast.showHttpError(error);
+            });
+          }
+        }
+      ]
+    }
+
+    this._dialog.open(DialogMessageComponent, { data });
   }
 }
